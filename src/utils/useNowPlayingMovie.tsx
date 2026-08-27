2ebdd40fe7b029611d5d2adfb3da233262632e35
Mahesh Kumar <vickymandal542@gmail.com>
2026-08-28 00:48:19 +0530

    fix: handle TMDB errors for now playing movies

--- src/utils/useNowPlayingMovie.tsx
+++ src/utils/useNowPlayingMovie.tsx
@@ -1,21 +1,29 @@
-import {NowPlayingMovieAPI,API_option} from "./constants";
-import {useEffect} from "react"
-import {useDispatch,useSelector} from "react-redux"
-import {addNowPlaying} from "../Slices/MovieSlice"
+import { NowPlayingMovieAPI, API_option } from "./constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNowPlaying } from "../Slices/MovieSlice";
+
 const useNowPlayingMovie = () => {
   const dispatch = useDispatch();
-  const NowPlaying = useSelector((store:any)=>store.movie?.NowPlayingMovie)
-  
-    // fetching the nowplaying movies
-  const fetchNowPlaying = async()=>{
-    const data = await fetch (NowPlayingMovieAPI,API_option);
-    const json = await data.json();
-    dispatch(addNowPlaying(json.results));
-  }
+  const NowPlaying = useSelector(
+    (store: any) => store.movie?.NowPlayingMovie
+  );
 
-  useEffect(()=>{
-    if(!NowPlaying) fetchNowPlaying();
+  const fetchNowPlaying = async () => {
+    try {
+      const data = await fetch(NowPlayingMovieAPI, API_option);
+      if (!data.ok) throw new Error(`TMDB request failed: ${data.status}`);
+      const json = await data.json();
+      dispatch(addNowPlaying(json.results ?? []));
+    } catch (error) {
+      console.error("Failed to fetch now playing movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!NowPlaying) fetchNowPlaying();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-}
+  }, []);
+};
+
 export default useNowPlayingMovie;
