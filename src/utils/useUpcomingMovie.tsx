ec7e65ec6150424022de4ccb9fcde3ddea6d910d
Mahesh Kumar <vickymandal542@gmail.com>
2026-08-28 00:48:13 +0530

    fix: correct upcoming selector and handle TMDB errors

--- src/utils/useUpcomingMovie.tsx
+++ src/utils/useUpcomingMovie.tsx
@@ -1,22 +1,29 @@
-import {UpcomingMoviesAPI,API_option} from "./constants";
-import {useEffect} from "react"
-import {useDispatch, useSelector} from "react-redux"
-import {addUpcomingMovie} from "../Slices/MovieSlice"
+import { UpcomingMoviesAPI, API_option } from "./constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUpcomingMovie } from "../Slices/MovieSlice";
+
 const useUpcomingMovie = () => {
   const dispatch = useDispatch();
-  const UpcomingMovie= useSelector((store:any)=>store.movie?.UpcomingMovie)
-  
-    // fetching the nowplaying movies
-  const fetchUpcomingMovie = async()=>{
-    const data = await fetch (UpcomingMoviesAPI,API_option);
-    const json = await data.json();
-    // console.log(json);
-    dispatch(addUpcomingMovie(json.results));
-  }
+  const UpcomingMovie = useSelector(
+    (store: any) => store.upcoming?.UpcomingMovie
+  );
 
-  useEffect(()=>{
-  if(!UpcomingMovie)  fetchUpcomingMovie();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-}
+  const fetchUpcomingMovie = async () => {
+    try {
+      const data = await fetch(UpcomingMoviesAPI, API_option);
+      if (!data.ok) throw new Error(`TMDB request failed: ${data.status}`);
+      const json = await data.json();
+      dispatch(addUpcomingMovie(json.results ?? []));
+    } catch (error) {
+      console.error("Failed to fetch upcoming movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!UpcomingMovie) fetchUpcomingMovie();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
 export default useUpcomingMovie;
