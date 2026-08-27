4fa187b5406a66a5f48855e7e51323558a51c696
Mahesh Kumar <vickymandal542@gmail.com>
2026-08-28 00:48:07 +0530

    fix: correct top rated selector and handle TMDB errors

--- src/utils/useTopRatedMovie.tsx
+++ src/utils/useTopRatedMovie.tsx
@@ -1,22 +1,29 @@
-import {TopRatedMoviesAPI,API_option} from "./constants";
-import {useEffect} from "react"
-import {useDispatch, useSelector} from "react-redux"
-import {addTopRatedMovie} from "../Slices/MovieSlice"
+import { TopRatedMoviesAPI, API_option } from "./constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTopRatedMovie } from "../Slices/MovieSlice";
+
 const useTopRatedMovie = () => {
   const dispatch = useDispatch();
-  const TopRatedMovie= useSelector((store:any)=>store.movie?.TopRatedMovie)
-  
-    // fetching the nowplaying movies
-  const fetchTopRatedMovie = async()=>{
-    const data = await fetch (TopRatedMoviesAPI,API_option);
-    const json = await data.json();
-    // console.log(json)
-    dispatch(addTopRatedMovie(json.results));
-  }
+  const TopRatedMovie = useSelector(
+    (store: any) => store.topRated?.TopRatedMovie
+  );
 
-  useEffect(()=>{
-   if(!TopRatedMovie) fetchTopRatedMovie();
-   // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-}
+  const fetchTopRatedMovie = async () => {
+    try {
+      const data = await fetch(TopRatedMoviesAPI, API_option);
+      if (!data.ok) throw new Error(`TMDB request failed: ${data.status}`);
+      const json = await data.json();
+      dispatch(addTopRatedMovie(json.results ?? []));
+    } catch (error) {
+      console.error("Failed to fetch top rated movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!TopRatedMovie) fetchTopRatedMovie();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
 export default useTopRatedMovie;
